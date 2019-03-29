a804228c635ebcd8d978499c9da1f4db5cc36070
Max Kuzmins <max@gameanalytics.com>
2019-03-29 08:01:06 +0000

    Mix to require Elixir 1.8.1.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirDruid.MixProject do
     [
       app: :elixir_druid,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.8.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
