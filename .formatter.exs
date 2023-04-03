5cb0c0e735e6fcf81a361f4760777d9c53454e1f
Matt McFarland <matthew.mcfarland@simplecast.com>
2023-04-03 10:51:56 -0600

    Add formatter export for `from/2`

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,11 @@
-# Used by "mix format"
+locals_without_parens = [
+  from: 2
+]
+
 [
-  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
+  ]
 ]
